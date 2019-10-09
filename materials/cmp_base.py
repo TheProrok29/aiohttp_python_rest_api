3f798c99ef3475878d525b48618e0d5bbff0594a
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-10-09 16:11:44 +0200

    Add check type of server response header
    * add materials folder with config file

--- materials/cmp_base.py
+++ materials/cmp_base.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-import requests
 import yarl
 
 CHUNK_SIZE = 4096
@@ -48,17 +46,4 @@ MP3 = (
 )
 
 if __name__ == '__main__':
-    download_dir = Path.home() / 'Pobrane' / 'birds_sample'
-    download_dir.mkdir(exist_ok=True)
-
-    with requests.Session() as session:
-        session.stream = True
-
-        for mp3_url in MP3:
-            with session.get(str(mp3_url)) as resp:
-                if resp.headers['Content-Type'] == 'audio/mpeg':
-                    mp3_file = download_dir / mp3_url.parts[-1]
-                    mp3_file.touch()
-                    with mp3_file.open('wb') as mp3_handle:
-                        for chunk in resp.iter_content(CHUNK_SIZE):
-                            mp3_handle.write(chunk)
+    pass
