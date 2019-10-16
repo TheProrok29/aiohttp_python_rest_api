8dc6334e07567d146a5994401fc7ea0533f8e056
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-10-16 10:47:58 +0200

    Add list_all samples API to user module

--- app/api/samples.py
+++ app/api/samples.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 import typing as tp
 
 from aiohttp import web
@@ -8,6 +9,15 @@ from app.model import bird_sample
 LOG = logging.getLogger(__name__)
 
 BIRD_SAMPLES: tp.List[bird_sample.BirdSample] = []
+for download_count, f in enumerate((Path.home() / 'Pobrane' / 'birds_sample').glob('*.mp3')):
+    if f.exists():
+        BIRD_SAMPLES.append(
+            bird_sample.BirdSample(
+                name=f.name.replace('.mp3', ''),
+                download_count=download_count,
+                path=f.absolute(),
+            ),
+        )
 
 
 # aiohttp.web.View
@@ -27,4 +37,4 @@ class BirdSamples(web.View):
 
 async def list_all(request: web.Request) -> web.Response:
     LOG.info('Getting a list of all bird samples')
-    raise web.HTTPNotImplemented()
+    return web.json_response(data=[bs.dict(exclude={'path'}) for bs in BIRD_SAMPLES])
